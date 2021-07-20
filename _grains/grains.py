f94ddb6f3236360cb939c29cd5e38088616c7abf
Max Mizikar <maxmzkr@censys.io>
2021-07-20 17:28:53 -0400

    add way to detect yubikey

--- _grains/grains.py
+++ _grains/grains.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import subprocess
+
 
 def main():
     grains = {}
-    grains["yubikey"] = False
+    p = subprocess.Popen("lsusb | grep 'Yubico'", shell=True)
+    p.communicate()
+    grains["yubikey"] = p.returncode == 0
     return grains
